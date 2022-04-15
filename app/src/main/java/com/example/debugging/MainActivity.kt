9a2c3474c5ec413f5fdd077355ca43a6453f5ffa
poyu <yoyohihirobert@gmail.com>
2022-04-16 01:34:31 +0800

    finish all practice requests

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.debugging
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
+        logging()
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+    fun logging() {
+        Log.v(TAG, "Hello, World!")
     }
 }
